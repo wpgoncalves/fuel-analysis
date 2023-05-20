8213d04defc3a54dda5bc09e80f3eb2e243c3874
Wesley Paulo Gonçalves <wpgoncalves1984@gmail.com>
2023-05-20 17:39:27 -0300

    Using custom exceptions and inserting useful information in addition to handling these custom exceptions.

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from pathlib import Path
 
 import streamlit as st
+from streamlit.elements import utils
 
+from custom_exceptions import ExcessValues
 from fuel_controller import FuelController
 from fuel_data import BASE_DIR, FuelData
 from settings import ABOUT_MSG, DATE_END, DATE_START
@@ -29,6 +31,8 @@ def load_data() -> FuelData:
 
 
 if __name__ == '__main__':
+
+    utils._shown_default_value_warning = True
 
     st.set_page_config(
         page_title='Projeto Integrador IV',
@@ -128,8 +132,10 @@ if __name__ == '__main__':
 
     # Containers separated by tabs: charts and spreadsheet
     with st.container():
-        tab1, tab2 = st.tabs(
-            [':bar_chart: Gráficos', ':clipboard: Planilha'])
+        tab1, tab2 = st.tabs([
+            ':bar_chart: Gráficos',
+            ':clipboard: Planilha'
+        ])
 
         with tab1:
             st.markdown(
@@ -213,25 +219,103 @@ if __name__ == '__main__':
 
                     )
 
-            st.markdown('# Valores Médios de Venda dos Combustíves')
-            st.markdown('* ## por Região Brasileira')
+            st.markdown('---')
+            st.markdown('### Valores Médios de Venda dos Combustíves')
 
-            fdt.get_chart_sales_value_by_region(st.pyplot)
+            with st.expander('**Comparativo por Região Brasileira**'):
+                st.markdown(
+                    '''
+                    > *O presente relatório apresenta os valores médios dos
+                    preços de venda dos combustíveis nas diferentes regiões
+                    do país, com base nos dados fornecidos pela Agência
+                    Nacional do Petróleo, Gás Natural e Biocombustíveis (ANP).
+                    O objetivo é fornecer uma visão geral da evolução dos
+                    preços dos principais tipos de combustíveis (gasolina,
+                    gasolina aditivada e etanol) no período selecionado, bem
+                    como comparar os valores médios entre as regiões
+                    brasileiras.*
+                    '''
+                )
+                fdt.get_chart_sales_value_by_region(st.pyplot)
 
-            if len(selected_regions) > 0:
-                st.markdown('* ## por Região e Estado Brasileiro')
-
+            with st.expander('**Comparativo por Estado e Região Brasileira**'):
+                st.markdown(
+                    '''
+                    > *Um dos fatores que influenciam o custo de vida dos
+                    brasileiros é o preço dos combustíveis. Esse preço
+                    varia de acordo com a região do país, a carga
+                    tributária, a concorrência entre os postos e a demanda
+                    dos consumidores. Neste contexto, vamos apresentar os
+                    valores médios dos preços de venda dos combustíveis nos
+                    diferentes estados, com base nos dados da Agência
+                    Nacional do Petróleo, Gás Natural e Biocombustíveis
+                    (ANP) referentes ao perído selecionado.*
+                    '''
+                )
                 fdt.get_chart_sales_value_by_regions_and_states(st.pyplot)
 
-            if len(selected_cities) > 0:
-                st.markdown('* ## por Cidade')
+            with st.expander('**Comparativo por Cidades Brasileiras**'):
+                st.markdown(
+                    '''
+                    > *Os valores médios dos preços de venda dos combustíveis
+                    nas diferentes cidades são calculados com base nas
+                    informações fornecidas pelos postos de abastecimento. Esses
+                    valores podem variar de acordo com a localização, a 
+                    concorrência, a oferta, a demanda, entre outros fatores.*
+                    '''
+                )
 
-                st.pyplot(fdt.get_chart_sales_value_by_cities())
+                try:
+                    st.pyplot(fdt.get_chart_sales_value_by_cities())
+                except ExcessValues as e:
+                    st.warning(e, icon="⚠️")
 
-            if len(selected_flags) > 0:
-                st.markdown('* ## por Bandeira')
+            with st.expander('**Comparativo de valores por bandeira dos postos\
+                             de combustível**'):
+                st.markdown(
+                    '''
+                    > *Um comparativo de valores por bandeira dos postos de
+                    combustível é uma forma de encontrar o melhor preço na hora
+                    de abastecer o seu veículo. Os preços dos combustíveis
+                    variam de acordo com a bandeira do posto, que representa a
+                    distribuidora que fornece o produto. As bandeiras mais
+                    conhecidas no Brasil são BR (Petrobras), Ipiranga, Shell e
+                    Ale. Existem também os postos sem bandeira, também chamados
+                    de bandeira branca que são aqueles que não respresentam uma
+                    marca própria, sendo livre para negociar e comprar
+                    combustíveis de diferentes distribuidoras.*
+                    '''
+                )
 
-                st.pyplot(fdt.get_chart_sales_value_by_flags())
+                try:
+                    st.pyplot(fdt.get_chart_sales_value_by_flags())
+                except ExcessValues as e:
+                    st.warning(e, icon="⚠️")
+
+            st.markdown('---')
+            st.markdown(
+                '### Evolução dos Valores Médios de Venda dos Combustíveis')
+
+            with st.expander('**Ao longo do tempo**'):
+                st.markdown(
+                    '''
+                    > *Os valores médios de venda dos combustíveis variam ao
+                    longo do tempo e são monitorados pela Agência Nacional do
+                    Petróleo, Gás Natural e Biocombustíveis (ANP). A ANP faz
+                    pesquisas semanais e mensais de preços de diferentes tipos
+                    de combustíveis e em várias localidades do país. Essas
+                    pesquisas mostram as mudanças nos preços e nas margens de
+                    comercialização dos combustíveis, e os efeitos das
+                    políticas públicas e do mercado no setor. Os dados das
+                    pesquisas podem ser acessados no site da ANP em formato
+                    aberto.*
+                    '''
+                )
+                st.pyplot(fdt.get_chart_evolution_of_sales_values_over_time())
+
+        # with tab2:
+        #     st.markdown('# Evolução dos Valores dos Combustíveis')
+        #     st.markdown('* ## ao longo do tempo')
 
         with tab2:
             columns = ['Revenda', 'CNPJ da Revenda', 'Produto',
